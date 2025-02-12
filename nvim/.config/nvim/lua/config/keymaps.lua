c16ccdd3fb47c2e063980f4da9163529c73308a6
PatrickHsiao <ptp_patrickhsiao@photoms.com.tw>
2025-02-12 15:38:52 +0800

    update neovim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,10 @@
-local map = vim.keymap.set
-map("n", "<A-h>", require("smart-splits").resize_left, { desc = "Resize split left" })
-map("n", "<A-j>", require("smart-splits").resize_down, { desc = "Resize split down" })
-map("n", "<A-k>", require("smart-splits").resize_up, { desc = "Resize split up" })
-map("n", "<A-l>", require("smart-splits").resize_right, { desc = "Resize split right" })
--- moving between splits
-map("n", "<C-h>", require("smart-splits").move_cursor_left, { desc = "Move to left split" })
-map("n", "<C-j>", require("smart-splits").move_cursor_down, { desc = "Move to down split" })
-map("n", "<C-k>", require("smart-splits").move_cursor_up, { desc = "Move to up split" })
-map("n", "<C-l>", require("smart-splits").move_cursor_right, { desc = "Move to right split" })
+-- local map = vim.keymap.set
+-- map("n", "<A-h>", require("smart-splits").resize_left, { desc = "Resize split left" })
+-- map("n", "<A-j>", require("smart-splits").resize_down, { desc = "Resize split down" })
+-- map("n", "<A-k>", require("smart-splits").resize_up, { desc = "Resize split up" })
+-- map("n", "<A-l>", require("smart-splits").resize_right, { desc = "Resize split right" })
+-- -- moving between splits
+-- map("n", "<C-h>", require("smart-splits").move_cursor_left, { desc = "Move to left split" })
+-- map("n", "<C-j>", require("smart-splits").move_cursor_down, { desc = "Move to down split" })
+-- map("n", "<C-k>", require("smart-splits").move_cursor_up, { desc = "Move to up split" })
+-- map("n", "<C-l>", require("smart-splits").move_cursor_right, { desc = "Move to right split" })
