a157f0fbef10ca5bdb75be1ff54616e78fc5862a
PatrickHsiao <ptp_patrickhsiao@photoms.com.tw>
2025-02-27 14:15:52 +0800

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
+if test -e /opt/homebrew/bin/brew 
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+end
+
 if status is-interactive
     set fish_tmux_default_session_name home
     set fish_tmux_autostart true
@@ -24,36 +28,6 @@ set -x MANROFFOPT -c
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
 fish_add_path ~/.local/bin ~/.local/scripts
-
-## Functions
-# Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
-function __history_previous_command
-    switch (commandline -t)
-        case "!"
-            commandline -t $history[1]
-            commandline -f repaint
-        case "*"
-            commandline -i !
-    end
-end
-
-function __history_previous_command_arguments
-    switch (commandline -t)
-        case "!"
-            commandline -t ""
-            commandline -f history-token-search-backward
-        case "*"
-            commandline -i '$'
-    end
-end
-
-if [ "$fish_key_bindings" = fish_vi_key_bindings ]
-    bind -Minsert ! __history_previous_command
-    bind -Minsert '$' __history_previous_command_arguments
-else
-    bind ! __history_previous_command
-    bind '$' __history_previous_command_arguments
-end
 
 # Fish command history
 function history
