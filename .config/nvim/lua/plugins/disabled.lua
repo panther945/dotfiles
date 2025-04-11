015adf345f4b5171b1476d45f844a1fd1debc9c8
PatrickHsiao <ptp_patrickhsiao@photoms.com.tw>
2025-04-11 13:37:24 +0800

    update

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
-  { "folke/persistence.nvim", enabled = false },
+  -- { "folke/persistence.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
