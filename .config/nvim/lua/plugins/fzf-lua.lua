5abb8fab29fecf7763568c8d10ce6f941e0b44d4
PatrickHsiao <ptp_patrickhsiao@photoms.com.tw>
2025-04-18 15:21:02 +0800

    update nvim

--- .config/nvim/lua/plugins/fzf-lua.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "ibhagwan/fzf-lua",
-  keys = {
-    { "<leader>ff", LazyVim.pick("files", { git_icons = false }), desc = "Find Files (Root Dir)" },
-    { "<leader>fF", LazyVim.pick("files", { root = false, git_icons = false }), desc = "Find Files (cwd)" },
-  },
-}
